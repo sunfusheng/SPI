78f0759ba9c01d50fbf1774b7b4797fa958797a3
sunfusheng <sfsheng0322@gmail.com>
2019-03-05 13:36:11 +0800

    add some modules

---
+++ spi-annotation/src/main/java/com/sunfusheng/spi/annotation/Provide.java
@@ -0,0 +1,15 @@
+package com.sunfusheng.spi.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author by sunfusheng on 2019/3/5
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.CLASS)
+public @interface Provide {
+    String value();
+}
