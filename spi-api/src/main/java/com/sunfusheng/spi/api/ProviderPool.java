f0496bb40bafefab46a73825f785319c079b8da1
sunfusheng <sfsheng0322@gmail.com>
2019-03-14 20:56:35 +0800

    解析注解信息

--- spi-api/src/main/java/com/sunfusheng/spi/api/ProviderPool.java
+++ spi-api/src/main/java/com/sunfusheng/spi/api/ProviderPool.java
@@ -1,7 +1,11 @@
 package com.sunfusheng.spi.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author by sunfusheng on 2019/3/14
  */
 public class ProviderPool {
+    private static Map<String, String> providers = new HashMap<>();
 }
