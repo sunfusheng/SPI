beb0bcb3f67eb9737e7c84908740d00410cf3fa3
sunfusheng <sfsheng0322@gmail.com>
2019-03-15 15:15:45 +0800

    解析注解信息

---
+++ module-b/src/main/java/com/sunfusheng/spi/module_b/BApplicationDelegate.java
@@ -0,0 +1,21 @@
+package com.sunfusheng.spi.module_b;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.sunfusheng.spi.annotation.Provide;
+import com.sunfusheng.spi.module.interfaces.AbsApplicationDelegate;
+
+/**
+ * @author by sunfusheng on 2019/3/14
+ */
+@Provide(AbsApplicationDelegate.class)
+public class BApplicationDelegate extends AbsApplicationDelegate {
+
+    private static final String TAG = "BApplicationDelegate";
+
+    @Override
+    public void onCreate(Application application) {
+        Log.d(TAG, "onCreate()");
+    }
+}
--- spi-compiler/src/main/java/com/sunfusheng/spi/compiler/ProvideProcessor.java
+++ spi-compiler/src/main/java/com/sunfusheng/spi/compiler/ProvideProcessor.java
@@ -4,9 +4,7 @@ import com.google.auto.service.AutoService;
 import com.sunfusheng.spi.annotation.Provide;
 
 import java.util.Collections;
-import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Filer;
@@ -16,10 +14,10 @@ import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.MirroredTypeException;
-import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 import javax.tools.Diagnostic;
 
@@ -29,9 +27,14 @@ import javax.tools.Diagnostic;
 @AutoService(Processor.class)
 public class ProvideProcessor extends AbstractProcessor {
 
-    private Filer mFiler; // 文件相关的辅助类
-    private Elements mElementUtils; // 元素相关的辅助类
-    private Messager mMessager; // 日志相关的辅助类
+    // 文件相关的辅助类
+    private Filer mFiler;
+
+    // 元素相关的辅助类
+    private Elements mElementUtils;
+
+    // 日志相关的辅助类
+    private Messager mMessager;
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnvironment) {
@@ -53,41 +56,56 @@ public class ProvideProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnvironment) {
-        if (annotations != null && !annotations.isEmpty()) {
-            Set<? extends Element> elements = roundEnvironment.getElementsAnnotatedWith(Provide.class);
-            if (elements != null && !elements.isEmpty()) {
-
-                for (Element element : elements) {
-                    TypeElement typeElement = (TypeElement) element;
-                    String packageName = mElementUtils.getPackageOf(element).getQualifiedName().toString();
-                    String className = typeElement.getQualifiedName().toString();
-                    String annotationValue = getProvideAnnotationValue(element);
-                    printNote("packageName: " + packageName);
-                    printNote("className: " + className);
-                    printNote("annotationValue: " + annotationValue);
-                }
-                return true;
-            }
+        if (annotations == null || annotations.isEmpty()) {
+            return false;
         }
-        return false;
+
+        Set<? extends Element> elements = roundEnvironment.getElementsAnnotatedWith(Provide.class);
+        if (elements == null || elements.isEmpty()) {
+            return false;
+        }
+
+        printNote("### Found providers, size is " + elements.size());
+        for (Element element : elements) {
+            if (filterElement(element)) {
+                continue;
+            }
+            TypeElement typeElement = (TypeElement) element;
+            String className = typeElement.getQualifiedName().toString();
+            String annotationValue = getProviderAnnotationValue(element);
+            printNote("### className: " + className);
+            printNote("### annotationValue: " + annotationValue);
+        }
+        return true;
     }
 
-    private String getProvideAnnotationValue(Element element) {
+    // 过滤被注解的元素
+    private boolean filterElement(Element element) {
+        if (element != null && element.getKind() == ElementKind.CLASS && element.getAnnotation(Provide.class) != null) {
+            return false;
+        }
+        return true;
+    }
+
+    // 获取注解值的全限定名
+    private String getProviderAnnotationValue(Element element) {
         try {
             Provide provide = element.getAnnotation(Provide.class);
             Class<?> clazz = provide.value();
             return clazz.getCanonicalName();
         } catch (MirroredTypeException mte) {
-            DeclaredType classDeclaredType = (DeclaredType) mte.getTypeMirror();
-            TypeElement classTypeElement = (TypeElement) classDeclaredType.asElement();
-            return classTypeElement.getQualifiedName().toString();
+            DeclaredType declaredType = (DeclaredType) mte.getTypeMirror();
+            TypeElement typeElement = (TypeElement) declaredType.asElement();
+            return typeElement.getQualifiedName().toString();
         }
     }
 
+    // 输出提示信息
     private void printNote(String msg, Object... args) {
         mMessager.printMessage(Diagnostic.Kind.NOTE, String.format(msg, args));
     }
 
+    // 输出错误信息
     private void printError(String msg, Object... args) {
         mMessager.printMessage(Diagnostic.Kind.ERROR, String.format(msg, args));
     }
